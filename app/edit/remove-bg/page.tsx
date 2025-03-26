0ceb312170fe8326a1a362da23b67f9ad4fabde5
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2025-03-26 21:44:45 +0530

    update remove bg

--- app/edit/remove-bg/page.tsx
+++ app/edit/remove-bg/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 
 import RemovedBgImage from "@/components/remove-background/remove-bg-image";
 import { ArrowLeft } from 'lucide-react';
@@ -8,7 +7,7 @@ const Page = () => {
   return (
     <div>
       <div className="h-screen flex justify-center items-center">
-        <div className="flex  justify-betweenv pb-5 relative">
+        <div className="flex pb-5 relative">
           <div className="absolute top-[-40]">
             <Link href={"/upload-image"}>
               <div className="flex gap-2 items-center justify-center text-white ">
